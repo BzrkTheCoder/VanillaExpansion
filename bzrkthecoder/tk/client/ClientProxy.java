2f84d5f2972d627c62c6fefda57e7a7fc793ab9f
BzrkTheCoder (Indie Dev) <bzrkthecoder@gmail.com>
2014-10-22 07:26:31 +0100

    Create ClientProxy.java

---
+++ bzrkthecoder/tk/client/ClientProxy.java
@@ -0,0 +1,13 @@
+package vanillaexpansion.bzrkthecoder.tk.client;
+
+import vanillaexpansion.bzrkthecoder.tk.CommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class ClientProxy extends CommonProxy {
+        
+        @Override
+        public void registerRenderers() {
+                // This is for rendering entities and so forth later on
+        }
+        
+}
